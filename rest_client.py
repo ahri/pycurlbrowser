7effacec23bdd540aa3f8ba42085a67e4305bfdd
Adam Piper <adam@ahri.net>
2011-08-31 15:45:55 +0100

    Factor out JSON

--- rest_client.py
+++ rest_client.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 from browser import Browser
 import pycurl
-import simplejson
+import simplejson as json
 
 class RestClient(Browser):
+
+    """
+    A simple REST client based upon pycurlbrowser
+    """
+
     def __init__(self, base):
         super(RestClient, self).__init__()
         self._curl.setopt(pycurl.USERAGENT, "pycurl.rest_client 0.1")
@@ -22,7 +27,7 @@ class RestClient(Browser):
     def get(self, obj, uid=None):
         self._curl.setopt(pycurl.HTTPGET, 1)
         self.go(obj, uid)
-        return simplejson.loads(self.src)
+        return self.src
 
     def head(self, obj, uid=None):
         self._curl.setopt(pycurl.HTTPGET, 1)
@@ -30,16 +35,34 @@ class RestClient(Browser):
         self.go(obj, uid)
         # TODO: care about headers
 
-    def post(self, obj, data):
+    def post(self, obj, data=None):
         self._curl.setopt(pycurl.POST, 1)
-        self._curl.setopt(pycurl.POSTFIELDS, simplejson.loads(data))
-        return simplejson.loads(self.src)
+        self._curl.setopt(pycurl.POSTFIELDS, data)
+        self.go(obj)
+        return self.src
 
-    def put(self, obj, uid, data):
+    def put(self, obj, uid, data=None):
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'PUT')
-        self._curl.setopt(pycurl.POSTFIELDS, simplejson.loads(data))
-        return simplejson.loads(self.src)
+        self._curl.setopt(pycurl.POSTFIELDS, data)
+        return self.src
 
     def delete(self, obj, uid):
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'DELETE')
         # TODO: care about headers
+
+class RestClientJson(RestClient):
+    """
+    A REST client that only speaks JSON
+    """
+
+    def get(self, obj, uid=None):
+        return json.loads(super(RestClientJson, self).get(obj, uid))
+
+    def post(self, obj, data=None):
+        return json.loads(super(RestClientJson, self).post(obj, json_dumps(data)))
+
+    def put(self, obj, uid, data=None):
+        return json.loads(super(RestClientJson, self).put(obj, uid, json_dumps(data)))
+
+    def delete(self, obj, uid):
+        return json.loads(super(RestClientJson, self).delete(obj, uid))
