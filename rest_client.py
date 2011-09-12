07a465f24f30b401b7adaccc52e6561c403aa140
Adam Piper <adam.piper@logica.com>
2011-09-12 15:30:23 +0100

    Change to normal "CRUD" instead of using HTTP's weird verbage

--- rest_client.py
+++ rest_client.py
@@ -4,35 +4,31 @@ import pycurl
 import simplejson as json
 
 class StatusInformational(Exception):
+
     """
     Represent 1xx status codes
     """
 
-    pass
-
 class StatusRedirection(Exception):
+
     """
     Represent 3xx status codes
     """
 
-    pass
-
 class StatusClientError(Exception):
+
     """
     Represent 4xx status codes
     """
 
-    pass
-
 class StatusServerError(Exception):
+
     """
     Represent 5xx status codes
     """
 
-    pass
-
 def status_factory(status):
-    if 100 <= status < 200:
+    if   100 <= status < 200:
         return StatusInformational()
     elif 300 <= status < 400:
         return StatusRedirection()
@@ -66,7 +62,15 @@ class RestClient(Browser):
             raise status_factory(res)
         return res
 
-    def get(self, obj, uid=None):
+    # CRUD
+
+    def create(self, obj, data=None):
+        self._curl.setopt(pycurl.CUSTOMREQUEST, 'POST')
+        self._curl.setopt(pycurl.POSTFIELDS, data)
+        self.go(obj)
+        return self.src
+
+    def read(self, obj, uid=None):
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'GET')
         self.go(obj, uid)
         return self.src
@@ -77,48 +81,43 @@ class RestClient(Browser):
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'HEAD')
         self.go(obj, uid)
 
-    def post(self, obj, data=None):
-        self._curl.setopt(pycurl.CUSTOMREQUEST, 'POST')
-        self._curl.setopt(pycurl.POSTFIELDS, data)
-        self.go(obj)
-        return self.src
-
-    def put(self, obj, uid, data=None):
+    def update(self, obj, uid, data=None):
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'PUT')
         self._curl.setopt(pycurl.POSTFIELDS, data)
         self.go(obj, uid)
         return self.src
 
-    def delete(self, obj, uid):
+    def destroy(self, obj, uid):
         # TODO: care about headers
         self._curl.setopt(pycurl.CUSTOMREQUEST, 'DELETE')
         self.go(obj, uid)
         return self.src
 
 class RestClientJson(RestClient):
+
     """
     A REST client that only speaks JSON
     """
 
-    def get(self, obj, uid=None):
-        return json.loads(super(RestClientJson, self).get(obj, uid))
-
-    def post(self, obj, data=None):
+    def create(self, obj, data=None):
         self._curl.setopt(pycurl.HTTPHEADER, ['Content-Type: text/json'])
-        res = super(RestClientJson, self).post(obj, json.dumps(data))
+        res = super(RestClientJson, self).create(obj, json.dumps(data))
         if len(res) > 0:
             return json.loads(res)
         return None
 
-    def put(self, obj, uid, data=None):
+    def read(self, obj, uid=None):
+        return json.loads(super(RestClientJson, self).read(obj, uid))
+
+    def update(self, obj, uid, data=None):
         self._curl.setopt(pycurl.HTTPHEADER, ['Content-Type: text/json'])
-        res = super(RestClientJson, self).put(obj, uid, json.dumps(data))
+        res = super(RestClientJson, self).update(obj, uid, json.dumps(data))
         if len(res) > 0:
             return json.loads(res)
         return None
 
-    def delete(self, obj, uid):
-        res = super(RestClientJson, self).delete(obj, uid)
+    def destroy(self, obj, uid):
+        res = super(RestClientJson, self).destroy(obj, uid)
         if len(res) > 0:
             return json.loads(res)
         return None
