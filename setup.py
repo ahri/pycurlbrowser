941732b0daa2aa2c118d709515dc5f91a60e1ecd
Adam Piper <adam@ahri.net>
2011-11-10 22:31:48 +0000

    Try to make it into a proper python module

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+PACKAGE = "pycurlbrowser"
+NAME = "pycurlbrowser"
+DESCRIPTION = "A minimal browser based on pycurl/lxml"
+AUTHOR = "Adam Piper"
+AUTHOR_EMAIL = "adam@ahri.net"
+URL = "https://github.com/ahri/pycurlbrowser"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read("README"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="AGPLv2",
+    url=URL,
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    platforms='any',
+    install_requires=[
+        'pycurl>=7.18',
+        'lxml>=2.3',
+        'simplejson>=2.2.1',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGLPv2 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False,
+)
