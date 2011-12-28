1656674b61fffe79260331fe023682217302e90a
Adam Piper <adam@ahri.net>
2011-12-28 19:52:41 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="pycurlbrowser",
-    version='0.2.0',
+    version='0.2.1',
     description="A minimal browser based on pycurl/lxml",
     long_description=open("README").read(),
     author="Adam Piper",
